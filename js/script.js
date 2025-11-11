d9b2e484b2a1ecd11a969a7f42eac9d8a847e4b2
Leonhard Weiler <Laser_Leo@hotmail.com>
2025-11-11 10:47:48 +0100

    first commit, getting it on github

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const beatlesOpen = document.getElementById("beatles__open");
+const beatlesDialog = document.getElementById("beatles__dialog");
+const beatlesClose = document.getElementById("beatles__close");
+
+beatlesOpen.addEventListener("click", () => {
+    beatlesDialog.showModal();
+});
+
+beatlesClose.addEventListener("click", () => {
+    beatlesDialog.close();
+});
+
+const bobdylanOpen= document.getElementById("bobdylan__open");
+const bobdylanDialog= document.getElementById("bobdylan__dialog");
+const bobdylanClose = document.getElementById("bobdylan__close");
+
+bobdylanOpen.addEventListener("click", () => {
+    bobdylanDialog.showModal();
+});
+
+bobdylanClose.addEventListener("click", () => {
+    bobdylanDialog.close();
+});
